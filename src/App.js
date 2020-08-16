0ee589ef9bae5101cbf22311ed202b6a28e8880d
David Giraldo <davidgiraldobueno@gmail.com>
2020-08-16 14:12:26 -0500

    Logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
 import 'tachyons';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/* <Logo />
+      <Logo />
+      {/* 
       <ImageLinkForm />
       <FaceRecognition /> */}
 
