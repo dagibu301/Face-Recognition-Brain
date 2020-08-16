2f86acbe93aac998d34d15acb9358191bc5069c5
David Giraldo <davidgiraldobueno@gmail.com>
2020-08-16 15:42:17 -0500

    particles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import React from 'react';
-import './App.css';
-import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank';
+import React from "react";
+import Particles from "react-particles-js";
+import "./App.css";
 
-import 'tachyons';
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+
+import "tachyons";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 60,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+  interactivity: {
+    events: {
+      onhover: { enable: true, mode: "repulse" },
+      onclick: { enable: true, mode: "push" },
+      resize: true,
+    },
+    modes: {
+      grab: { distance: 400, line_linked: { opacity: 1 } },
+      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+      repulse: { distance: 100, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 },
+    },
+  }
+};
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={particlesOptions} />
       <Navigation />
       <Logo />
       <Rank />
@@ -17,9 +46,10 @@ function App() {
       {/* 
       
       <FaceRecognition /> */}
-
     </div>
   );
 }
 
 export default App;
+
+
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,12 +1,13 @@
 import React from "react";
+import "./Rank.css";
 
 const Rank = () => {
   return (
-    <div>
+    <div className="rank-text white f3">
         <div className="white f3">
             {'David, your current rank is...'}
         </div>
-        <div className="white f1">
+        <div className=" white f1">
             {' #5'}
         </div>
     </div>
