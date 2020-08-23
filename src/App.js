e34441201da4653e13fcc1ddd9ed656d993a5e48
David Giraldo <davidgiraldobueno@gmail.com>
2020-08-22 22:21:49 -0500

    serve

--- src/App.js
+++ src/App.js
@@ -108,7 +108,7 @@ class App extends Component {
 
   onPictureSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    fetch("http://localhost:3001/imageurl", {
+    fetch("https://frozen-inlet-49147.herokuapp.com/imageurl", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -118,7 +118,7 @@ class App extends Component {
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch("http://localhost:3001/image", {
+          fetch("https://frozen-inlet-49147.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   };
 
   onsubmitSignIn = () => {
-    fetch("http://localhost:3001/register", {
+    fetch("https://frozen-inlet-49147.herokuapp.com/register", {
       method: 'post',
       headers: { 'Content-Type': 'application/json'},
       body: JSON.stringify({
